8271391a8fd6da1d043eff324ef475cdcee34553
VictorRodriguezContact <victorrodriguezcontact@gmail.com>
2025-03-04 18:38:17 +0000

    Project creation

---
+++ src/test/java/com/technica_studio/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technica_studio.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
